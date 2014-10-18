da0e07920a96b8b7a176727d7659d19b35ca0784
Aharon h. <aharon.topq@gmail.com>
2014-10-19 01:32:13 +0300

    loop over players

--- src/main/java/pi/arcade/simon/SimonGame.java
+++ src/main/java/pi/arcade/simon/SimonGame.java
@@ -1,19 +1,47 @@
 package pi.arcade.simon;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.collect.Iterables;
+
 import pi.arcade.Game;
 import pi.arcade.Player;
 
 public class SimonGame extends Game {
 
-	final Player[] players;
-	Player currPlayer;
+	private final List<SimonPlayer> players;
+	private final Iterator<SimonPlayer> playersIterator;
+	protected SimonPlayer currPlayer;
 
 	public SimonGame(Player... player) {
+		players = new ArrayList<SimonPlayer>();
 		for (Player p : player) {
+			SimonPlayer simonPlayer;
 			if (!(p instanceof SimonPlayer)) {
-
+				simonPlayer = new SimonPlayer(p);
+			} else {
+				simonPlayer = (SimonPlayer) p;
 			}
+			players.add(simonPlayer);
 		}
+		playersIterator = Iterables.cycle(players).iterator();
+	}
+
+	@Override
+	protected Player currentPlayer() {
+		return currPlayer;
+	}
+
+	@Override
+	protected void turnEnd() {
+		currPlayer = playersIterator.next();
+	}
+
+	@Override
+	protected boolean isGameEnd() {
+		return false;
 	}
 
 }
