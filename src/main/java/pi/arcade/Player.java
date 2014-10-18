f9988355680e03c696bcdc6db2080ff96ca75ad6
Aharon h. <aharon.topq@gmail.com>
2014-10-19 01:11:43 +0300

    new copy c'tor

--- src/main/java/pi/arcade/Player.java
+++ src/main/java/pi/arcade/Player.java
@@ -6,11 +6,22 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioProvider;
 
 public abstract class Player extends Panel {
+	private final GpioController gpio;
+	private final GpioProvider providerForButtons, providerForLEDs;
+
 	public Player(GpioController gpio, GpioProvider providerForButtons,
 			String player, GpioProvider providerForLEDs) {
 		super(gpio, providerForButtons, providerForLEDs);
 		setName(player);
+		this.gpio = gpio;
+
+		this.providerForButtons = providerForButtons;
+		this.providerForLEDs = providerForLEDs;
 	}
 
-	public abstract boolean turn();
+	public Player(Player p) {
+		this(p.gpio, p.providerForButtons, p.getName(), p.providerForLEDs);
+	}
+
+	public abstract boolean turn() throws InterruptedException;
 }
